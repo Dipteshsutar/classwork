a18b92b61528a7c99224bc4caccb40da8c145526
Diptesh <108788659+Dipteshsutar@users.noreply.github.com>
2022-07-26 17:18:28 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+a=int(input("enter first number :"))
+o=str(input("enter oprater :"))
+b=int(input("enter second number :"))
+
+match o:
+    case '+':
+        print(f"{a} {o} {b} = {a+b}")
+    case '-':
+         print(f"{a} {o} {b} = {a-b}")
+    case '/':
+         print(f"{a} {o} {b} = {a/b}")
+    case '*':
+         print(f"{a} {o} {b} = {a*b}")
+    case _:
+        print('please enter valid oprater ')
